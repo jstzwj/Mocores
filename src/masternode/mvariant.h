4c5a5d10eb2d1d8d862d3dabdcae228e1e132261
WangJun <1103870790@qq.com>
2017-03-29 00:30:59 +0800

    Add mvariant

--- src/masternode/mvariant.h
+++ src/masternode/mvariant.h
@@ -2,99 +2,205 @@
 #define MVARIANT_H
 #include<type_traits>
 #include<utility>
+#include<memory>
 #include<cstdint>
 #include"errcode.h"
 namespace mocores
 {
+    template<class T>
+    class VarTag
+    {
+        using type=T;
+    };
+
+    template<class U,class T,class ...Targs>
+    class HasType
+    {
+    public:
+        constexpr static bool value=false||HasType<U,Targs...>::value;
+    };
+    template<class U,class ...Targs>
+    class HasType<U,U,Targs...>
+    {
+    public:
+        constexpr static bool value=true||HasType<U,Targs...>::value;
+    };
+    template<class U,class T>
+    class HasType<U,T>
+    {
+    public:
+        constexpr static bool value=false;
+    };
+    template<class U>
+    class HasType<U,U>
+    {
+    public:
+        constexpr static bool value=true;
+    };
+
+    template<class U,class T,class ...Targs>
+    class GetIndex
+    {
+    public:
+        const static int index=GetIndex<U,Targs...>::index+1;
+    };
+    template<class U,class ...Targs>
+    class GetIndex<U,U,Targs...>
+    {
+    public:
+        const static int index=0;
+    };
+    template<class U,class T>
+    class GetIndex<U,T>
+    {
+    public:
+        const static int index=-1;
+    };
+    template<class U>
+    class GetIndex<U,U>
+    {
+    public:
+        const static int index=0;
+    };
 
     template<class T,class ...Targs>
-    class MVariant:public MVariant<Targs...>
+    class Destruct
     {
     public:
-        MVariant():data(nullptr){}
-        MVariant(const T & value)
+        static void destruct(int index,void * ptr)
         {
-            data=new T(value);
-            curindex=type_index;
-        }
-
-        ~MVariant()
-        {
-            delete data;
-        }
-        constexpr uint32_t index()const {return curindex;}
-
-        T& get()
-        {
-            if(data!=nullptr)
-                return *data;
+            if(index==0)
+            {
+                delete (static_cast<T *>(ptr));
+            }
             else
-                throw MOCORES_VARIANT_EMPTY;
+            {
+                Destruct<Targs...>::destruct(index-1,ptr);
+            }
         }
-        void set(const T& val)
-        {
-            if(data!=nullptr)
-                *data=val;
-            else
-                throw MOCORES_VARIANT_EMPTY;
-        }
-
-    protected:
-        T * data;
-        static uint32_t curindex;
-        constexpr static uint32_t type_index=MVariant<Targs...>::type_index+1;
     };
-
-
     template<class T>
-    class MVariant<T>
+    class Destruct<T>
     {
     public:
-        MVariant():data(nullptr){}
-        MVariant(const T & value)
+        static void destruct(int index,void * ptr)
         {
-            data=new T(value);
-            curindex=type_index;
+            if(index==0)
+            {
+                delete (static_cast<T *>(ptr));
+            }
+            else
+            {
+                throw "Can not find type.";
+            }
+        }
+    };
+
+    template<class T,class ...Targs>
+    class Copy
+    {
+    public:
+        static void copy(int index,void *& source,void *& to)
+        {
+            if(index==0)
+            {
+                if(source!=to)
+                    to=new T(*(static_cast<T *>(source)));
+            }
+            else
+            {
+                Copy<Targs...>::copy(index-1,source,to);
+            }
+        }
+    };
+    template<class T>
+    class Copy<T>
+    {
+    public:
+        static void copy(int index,void *& source,void *& to)
+        {
+            if(index==0)
+            {
+                if(source!=to)
+                    to=new T(*(static_cast<T *>(source)));
+            }
+            else
+            {
+                throw "Can not find type.";
+            }
+        }
+    };
+
+
+    template<class T,class ...Targs>
+    class MVariant
+    {
+    public:
+        MVariant()
+        {
+            data=new T();
+            curindex=0;
+        }
+        template<class U>
+        MVariant(const U & value)
+        {
+            if(HasType<U,T,Targs...>::value==true)
+            {
+                data=new U(value);
+                curindex=GetIndex<U,T,Targs...>::index;
+            }
+            else
+            {
+                throw "Unknown type.";
+            }
+        }
+        template<class U,class ...args>
+        MVariant(MVariant<U,args...>& other)
+        {
+            Copy<U,args...>::copy(other.curindex,other.data,this->data);
+            this->curindex=other.curindex;
         }
 
-        ~MVariant()
+        template<class U,class ...args>
+        MVariant& operator =(MVariant<U,args...>& other)
         {
-            delete data;
+            Copy<U,args...>::copy(other.curindex,other.data,this->data);
+            this->curindex=other.curindex;
+        }
+
+        virtual ~MVariant()
+        {
+            if(data!=nullptr)
+            {
+                Destruct<T,Targs...>::destruct(curindex,data);
+            }
         }
         constexpr uint32_t index()const {return curindex;}
 
-
-        T& get()
+        template<class U>
+        void operator =(const U& val)
         {
             if(data!=nullptr)
-                return *data;
-            else
-                throw MOCORES_VARIANT_EMPTY;
+            {
+                Destruct<T,Targs...>::destruct(curindex,data);
+            }
+            data=new U(val);
+            curindex=GetIndex<U,T,Targs...>::index;
         }
-        void set(const T& val)
-        {
-            if(data!=nullptr)
-                *data=val;
-            else
-                throw MOCORES_VARIANT_EMPTY;
-        }
-    protected:
-        T * data;
-        static uint32_t curindex;
-        constexpr static uint32_t type_index=0;
+        void * data;
+        int curindex;
     };
-
 
     template <class T, class U,class... Types>
-    constexpr T& get(MVariant<U,Types...>& v)
+    T get(MVariant<U,Types...>& v)
     {
-        if(v.data!=nullptr)
+        if(GetIndex<T,U,Types...>::index==v.curindex)
         {
-            return *(v.data);
+            return *(static_cast<T *>(v.data));
         }
         else
         {
-            return *(dynamic_cast<MVariant<Types...>&>(v).data);
+            throw "Can not find the value.";
         }
     }
 
--- test/mocores_test/main.cpp
+++ test/mocores_test/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include<string>
 #include"../../src/masternode/mvariant.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    mocores::MVariant<int,double,bool> var;
+    mocores::MVariant<int,double,std::string> var=std::string("123");
+    cout << mocores::get<std::string>(var) << endl;
     var=1;
     cout << mocores::get<int>(var) << endl;
+    var=1.0;
+    cout << mocores::get<double>(var) << endl;
     return 0;
 }
