8f1c7f24ca5866a38743195458d4c7b3508796bd
WangJun <1103870790@qq.com>
2018-09-19 21:51:13 +0800

    update

---
+++ include/mocores/common/node_id.h
@@ -0,0 +1,30 @@
+#include <cstdint>
+#include <ctime>
+#include <string>
+
+namespace mocores
+{
+    class NodeID
+    {
+    public:
+        std::string host;
+        std::uint16_t port;
+        std::uint64_t epoch;
+
+        std::string to_string()
+        {
+
+        }
+
+
+        void parse_string(const std::string &node_id)
+        {
+
+        }
+
+        std::uint32_t hash()
+        {
+            
+        }
+    };
+}
