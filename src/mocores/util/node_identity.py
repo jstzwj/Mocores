4d2f76f4a9e28c681e46fda5ef7430048c1707fd
WangJun <1103870790@qq.com>
2018-09-19 18:00:43 +0800

    update

--- src/mocores/util/node_identity.py
+++ src/mocores/util/node_identity.py
@@ -1,18 +1,26 @@
 
 
-def get_node_id(ip, port, start_time):
-    return '{0}:{1}:{2}'.format(
-            str(ip), 
-            str(port), 
-            str(start_time))
+class NodeID(object):
+    def __init__(self, ip, port, start_time):
+        self.ip = ip
+        self.port = port
+        self.start_time = start_time
 
-def get_node_info_by_id(id):
+    def to_string(self):
+        return '{0}:{1}:{2}'.format(
+            str(self.ip), 
+            str(self.port), 
+            str(self.start_time))
+
+    def hash(self):
+        id = self.to_string()
+        hash = 0
+        for c in id:
+            hash = hash + ord(c)
+            hash = hash % (2^32)
+        return hash
+
+
+def parse_node_id(id):
     info = id.split(':')
-    return info[0], info[1], info[2]
-
-def get_node_id_hash(id):
-    hash = 0
-    for(c in id):
-        hash += ord(c)
-        hash %= 65536
-    return hash
+    return NodeID(info[0], info[1], info[2])
