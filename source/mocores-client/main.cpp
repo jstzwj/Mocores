67f09d5f2b94f3c2a33961d6335856214c60c451
JunWang <1103870790@qq.com>
2018-09-20 15:43:40 +0800

    update cmake

--- source/mocores-client/main.cpp
+++ source/mocores-client/main.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    BOOST_LOG_TRIVIAL(info) << "Mocores client starts";
+    BOOST_LOG_TRIVIAL(info) << "Mocores client is starting.";
     std::cout<<"hello world"<<std::endl;
+	BOOST_LOG_TRIVIAL(info) << "Mocores client is existing.";
     return 0;
 }
