2005dbc9dc541ce121008b7c619f088ba11d1a79
JunWang <1103870790@qq.com>
2018-10-16 18:27:39 +0800

    update

--- python/demo.py
+++ python/demo.py
@@ -10,19 +10,17 @@ class MyActor(object):
         print(message)
         return 1
 
-    def __del__(self):
-        print("del")
-
 
 async def main():
     client = mocores.Client("192.168.1.1", 60006)
-    client.connect("192.168.1.1", 60006)
+    client.connect("192.168.1.2", 60006)
 
     my_actor = client.get_actor(MyActor, 1)
     num = await my_actor.hello_world(message="hello world")
     print(num)
 
+
+# async run main
 loop = asyncio.get_event_loop()
-# Blocking call which returns when the hello_world() coroutine is done
 loop.run_until_complete(main())
 loop.close()
